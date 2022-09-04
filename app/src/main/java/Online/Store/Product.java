92645290e2034abe164e6ce8ebaccb8c6457d83a
Natalia H <nholea@gmail.com>
2022-09-04 17:11:02 +0200

    Add Product class

---
+++ app/src/main/java/Online/Store/Product.java
@@ -0,0 +1,38 @@
+package Online.Store;
+
+public class Product {
+    private final String name;
+    private final String productImage;
+
+    private final Description description;
+    private final Double price;
+    private final String reference;
+
+    public Product(String name, String productImage, Description description, Double price, String reference) {
+        this.name = name;
+        this.productImage = productImage;
+        this.description = description;
+        this.price = price;
+        this.reference = reference;
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    public String getProductImage(){
+        return productImage;
+    }
+
+    public Description getDescription(){
+        return description;
+    }
+
+    public Double getPrice(){
+        return price;
+    }
+
+    public String getReference(){
+        return reference;
+    }
+}
