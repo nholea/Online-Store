1e8db6e8b0260d8abd7365e312583f3c91f605e4
Natalia H <nholea@gmail.com>
2022-09-04 17:12:13 +0200

    Add Description class

---
+++ app/src/main/java/Online/Store/Description.java
@@ -0,0 +1,21 @@
+package Online.Store;
+
+public class Description {
+
+    private final String description;
+    private final String attribute;
+
+    public Description(String description, String attribute) {
+        this.description = description;
+        this.attribute = attribute;
+    }
+
+
+    public String getDescription() {
+        return description;
+    }
+
+    public String getAttribute() {
+        return attribute;
+    }
+}
