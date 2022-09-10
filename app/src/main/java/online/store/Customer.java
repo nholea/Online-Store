b7e61306f3caf8b758fcd05953dd4fece49170b8
Natalia H <nholea@gmail.com>
2022-09-10 12:13:00 +0200

    refactor

--- app/src/main/java/online/store/Customer.java
+++ app/src/main/java/online/store/Customer.java
@@ -4,7 +4,8 @@ public class Customer {
 
     public static void main(String[] args){
         ProductStorage productStorage = new ProductStorage();
-        Store store = new Store(productStorage);
+        OutputProductFormat outputProductFormat = new OutputProductFormat(productStorage);
+        Store store = new Store(outputProductFormat);
         store.showProductCatalogue();
     }
 }
