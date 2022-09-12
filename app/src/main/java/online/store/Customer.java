b97ad8492708f10efe38fc97d82aa14edaabde40
Natalia H <nholea@gmail.com>
2022-09-12 22:24:47 +0200

    show product details by reference

--- app/src/main/java/online/store/Customer.java
+++ app/src/main/java/online/store/Customer.java
@@ -1,9 +1,23 @@
 package online.store;
 
+import java.util.Scanner;
+
 public class Customer {
 
     public static void main(String[] args){
         Store store = new Store();
         store.showProductCatalogue();
+
+        System.out.println("Which product would you like to explore? - Introduce a product's reference.");
+        Scanner input = new Scanner(System.in);
+        String inputReference = input.nextLine();
+
+        store.showProductDetailsByReference(inputReference);
+
+        int inputChoice = input.nextInt();
+
+        if (inputChoice == 2) {
+            store.showProductCatalogue();
+        }
     }
 }
