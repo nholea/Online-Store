b6da9f72646764071ae869b9c890daf34d6225ed
Natalia H <nholea@gmail.com>
2022-09-14 22:34:56 +0200

    feat: filter Products by price

--- app/src/main/java/online/store/Customer.java
+++ app/src/main/java/online/store/Customer.java
@@ -5,11 +5,8 @@ import java.util.Scanner;
 public class Customer {
 
     public static void main(String[] args){
-        Store store = new Store();
         CustomerChoice customerChoice = new CustomerChoice();
-        store.showProductCatalogue();
-
-        customerChoice.ProductDetails();
+        customerChoice.filterProductsByPrice();
 
     }
 }
